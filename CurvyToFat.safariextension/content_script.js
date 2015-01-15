5440246428ccea0e9331fead435a5c691d6cc8ea
J-F B <dontbelievethebyte@gmail.com>
2015-01-15 10:40:13 -0500

    First commit

---
+++ CurvyToFat.safariextension/content_script.js
@@ -0,0 +1,61 @@
+var titleAssigned = false;
+var title = document.title;
+
+var observer = new MutationObserver(function() {
+	walk(document.body);
+	if (title != document.title){
+		handleTitle();
+	}
+});
+
+observer.observe(
+	document.body,
+	{
+		childList: true
+	}
+);
+
+function walk(node){
+	var child, next;
+
+	switch ( node.nodeType ){
+		case 1:  // Element
+		case 9:  // Document
+		case 11: // Document fragment
+			child = node.firstChild;
+			while (child){
+				next = child.nextSibling;
+				walk(child);
+				child = next;
+			}
+			break;
+		case 3: // Text node
+			if (undefined == node.curvyToFat) {
+				node.curvyToFat = 'yes';
+				handleTextNode(node);
+			}
+			break;
+	}
+}
+
+function handleTextNode(textNode){
+	textNode.nodeValue = fixText(textNode.nodeValue);
+}
+
+function handleTitle(){
+	if (!titleAssigned){
+		titleAssigned = true;
+		document.title = fixText(document.title);
+	}
+}
+
+function fixText(text){
+	text = text.replace(/(^|\b)fat($|\b)/g, "honest");
+	text = text.replace(/(^|\b)Fat($|\b)/g, "Honest");
+	text = text.replace(/(^|\b)Curvy($|\b)/g, "Fat");
+	text = text.replace(/(^|\b)curvy($|\b)/g, "fat");
+	return text;
+}
+
+walk(document.body);
+handleTitle();
---
+++ README.md
@@ -0,0 +1,21 @@
+### Curvy to Fat
+
+![Curvy to fat](/src/icon-128.png?raw=true "Curvy To Fat")
+
+Safari extension that replaces occurrences of 'curvy' with 'fat' and 'fat' with 'honest'.
+
+Chrome version is [here](https://github.com/DontBelieveTheByte/CurvyToFa) or on the [Play store]().
+
+Firefox version is [here](https://github.com/DontBelieveTheByte/CurvyToFat-firefox) or on the [Mozilla Add-Ons website](https://addons.mozilla.org/en-US/firefox/addon/curvytofat/).
+
+### Manual Installation
+
+1. Download the [extension]([Direct download of crx file](/CurvyToFat.crx?raw=true), open Safari, enable Developer mode.
+2. Choose Show Extension Builder from the Develop menu.
+3. Choose Add Extension... from the plus button.
+
+### Example
+
+This is what this page would look like
+
+![Screenshot](/screenshot.png?raw=true "Screenshot")
